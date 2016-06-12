e9b07800235bf65d42640928c0626bd7e7d7907d
tomkoker <teddy.koker@gmail.com>
2016-06-12 16:09:34 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Photocell Graph
+Graph a live feed of RCtime readings from a photocell
+ - 1 uF capacitor
+ - 5-20 kO photocell
---
+++ server.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var GPIO = require('onoff').Gpio;
+
+// Pins
+var pin = 18;
+
+var recentValue = RCtime(pin);
+
+function RCtime(pin) {
+  var rcpin = new GPIO(pin, 'out');
+  rcpin.writeSync(0);
+
+
+  rcpin = new GPIO(pin, 'in');
+  var start = new Date();
+  while(rcpin.readSync() == 0){
+    //do nothing
+  }
+  return new Date() - start;
+}
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('New connection ' + socket.id);
+  socket.on('get', function(){
+    socket.emit('reading', {value: recentValue});
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+
+
+setInterval(function(){
+  recentValue = RCtime(pin);
+},400);
+
